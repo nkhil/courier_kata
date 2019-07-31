100eeaa17363cd1ff5d0f9158b60d0d45e29373e
Nikhil Vijayan <nkhilv@gmail.com>
2019-07-31 22:22:35 +0100

    chore: add prices file

---
+++ prices.js
@@ -0,0 +1,17 @@
+module.exports = {
+  SMALL: {
+    maxDimensions: 10,
+    cost: 300
+  },
+  MEDIUM: {
+    maxDimensions: 50,
+    cost: 800
+  },
+  LARGE: {
+    maxDimensions: 100,
+    cost: 1500
+  },
+  EXTRA_LARGE: {
+    cost: 2500 // no maxDimensions property
+  }
+};
